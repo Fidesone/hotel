823310042a0af4b2dcb9f11bcdbbe23ff8b53039
Héctor González Pereda <hgonza02@ucm.es>
2021-04-01 20:23:42 +0200

    add header and  css sass

---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+
+<?php
+
+echo "Esto es el Header" . "<br>";
+
+
+?>
+
+<div id='container'>
+    <h1>Barra de navegacion</h1>
+    <nav>
+    <a  class='nav-link' href='#'> Inicio/Login </a>
+    <a  class='nav-link' href='#'> Habitaciones</a>
+    <a  class='nav-link' href='#'> Precio </a>
+    <a  class='nav-link' href='#'> Contacto </a>
+    <nav>
+</div>
--- index.php
+++ index.php
@@ -1,16 +1,18 @@
-<html>
-
-<p>esto es html</p>
-
+<!DOCTYPE html>
 <?php
+
 include_once("connection.php");
 echo "La conexión ha sido exitosa" . "<br>";
-
-
+include ("header.php");
 
 echo "esto es php";
 
 ?>
+
+<head>
+<link href="sass/header.css" rel="stylesheet" type="text/css">
+</head>
+
 
 
 </html>
