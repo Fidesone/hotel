85084031dd1f9a894bb612fd5252398e13523c44
Héctor González Pereda <hgonza02@ucm.es>
2021-05-03 20:25:31 +0200

    add images rooms 1, 2, 3

--- form_reservation.php
+++ form_reservation.php
@@ -10,9 +10,20 @@ include_once 'connection.php';
 <body>
     <h1>Reservar una habitación</h1>
     <div class="container_rooms">
-    <div class="room">1. Individual</div> 
-    <div class="room">2. Doble</div> 
-    <div class="room">3. Suite</div> 
+    <div class="room1">
+        <img src="img/hab_individual.jpg"></img>
+        <h2> 1. Individual</h2>
+    </div> 
+    <br></br>
+    <div class="room2">
+        <img src="img/hab_doble.jpg"></img>
+        <h2> 2. Doble</h2>
+    </div> 
+    <br></br>
+    <div class="room3">
+        <img src="img/hab_suite.jpg"></img>
+        <h2> 3. Suite</h2>
+    </div> 
 
     </div>
 </body>
