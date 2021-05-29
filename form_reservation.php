e926d1fab3f3e1856a9f4e0bb860793417594af3
Héctor González Pereda <51021644+Fidesone@users.noreply.github.com>
2021-05-29 13:12:18 +0200

    css form_reservation.php

--- form_reservation.php
+++ form_reservation.php
@@ -1,5 +1,6 @@
 <?php
 include_once 'connection.php';
+include 'header.php';
 
 session_start();
 
@@ -15,10 +16,13 @@ session_start();
 <DOCTYPE html>
 <html>
 <head>
+<link href="sass/header.css" rel="stylesheet" type="text/css">
 <link href="sass/form_reservation.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <h1>Reservar una habitación</h1>
+    <h2>Elige alguna entre los diferentes tipos:</h2>
+
     <div class="container_rooms">
     <?php
         $query= $conn->prepare("SELECT * FROM habitacion");
@@ -26,14 +30,18 @@ session_start();
         while($row = $query->fetch()){
     ?>
     <div class="room1">
+    <div class='login_form'>
         <img src="<?php echo $row['imagen'] ?>"></img>
             <div>
+                <div class='individual'>
                 <h2><?php echo $row['nombre'] ?></h2>
                 <h3><?php echo $row['descrip'] ?></h3>
+                </div>
+            
             <form action="insert_reservation.php" method="post" class="container_form">
                 <input type="hidden" name="id_habitacion" value="<?php echo $row['id_habitacion']?>">
                 <h2 class="container_newuser">Registrar una nueva habitación</h2>
-                <h3 class="container_newuser">Datos personales del cliente</h3>
+                <h3 class="container_newuser">Datos personales:</h3>
 
                 <p>Nombre del cliente </p>
                 <input type="text" name='nombre_cliente'>  
@@ -47,13 +55,9 @@ session_start();
                 <input type="date" name='fecha_fin' placeholder='Fecha Fin'> 
                 <input type="submit" name='registrar'>
             </form> 
-        </div>
-
-        <!-- <form action="insert_reservation.php" method="post" class="container_form">
-    <h2 class="container_newuser">Registrar una nueva Reserva</h2>
         
-        <input type="submit" name='registrar'>
-    </form> -->
+        </div>
+        </div>
         
     </div> 
     <br></br>
